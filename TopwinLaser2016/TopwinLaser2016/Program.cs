ba39fcd691c185d4c985b1204db27ee039917d35
WHB <wenwenbeibei99@outlook.com>
2016-03-23 10:08:24 +0800

    Test

--- TopwinLaser2016/TopwinLaser2016/Program.cs
+++ TopwinLaser2016/TopwinLaser2016/Program.cs
@@ -17,6 +17,8 @@ namespace TopwinLaser2016
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMainWindow());
+
+            //test
         }
     }
 }
